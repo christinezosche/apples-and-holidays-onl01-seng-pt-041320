f4aca89d04a8ed46fc75a1bf86791fea88432c73
Christine Zosche <christine.zosche@gmail.com>
2020-04-26 20:28:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
 holiday_hash.each do |season_key, holiday_details_hash|
   if season_key == season
-    holiday_details_hash[:holiday_name] = supply_array
+    holiday_details_hash[:"#{holiday_name}"] = supply_array
   end
 end
 
